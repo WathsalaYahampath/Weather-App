d74a6a96763cba921b1cdba5ba5b758cc4e51f33
WathsalaYahampath <yahampa-se19025@stu.kln.ac.lk>
2023-09-11 17:15:52 +0530

    Getting weather api

--- weather-app/src/components/dist/api.dev.js
+++ weather-app/src/components/dist/api.dev.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.GEO_API_URL = exports.geoApiOptions = void 0;
+exports.WEATHER_API_KEY = exports.WEATHER_API_URL = exports.GEO_API_URL = exports.geoApiOptions = void 0;
 var geoApiOptions = {
   method: 'GET',
   headers: {
@@ -14,4 +14,8 @@ var geoApiOptions = {
 exports.geoApiOptions = geoApiOptions;
 var GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 exports.GEO_API_URL = GEO_API_URL;
+var WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+exports.WEATHER_API_URL = WEATHER_API_URL;
+var WEATHER_API_KEY = "6720c47f0670942154dd846123cf25d5";
+exports.WEATHER_API_KEY = WEATHER_API_KEY;
 //# sourceMappingURL=api.dev.js.map
